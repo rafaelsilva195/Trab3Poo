246934482991ade00767014fb4e58161728625cc
Rafael Bueno da Silva <rafaelzuum@hotmail.com>
2015-06-01 23:52:20 -0300

    Update IBooks.java

--- IBooks.java
+++ IBooks.java
@@ -1,10 +1,7 @@
 package biblioteca;
 
 public interface IBooks {
+	public void setCod(int cod);
 	public String nameBook();
-	public int numOfEmp();
-	public void setNameBook(String name);
-	public void setNumOfEmp(int num);
-	public void setUser(String user);
-	public String getUser();
+	public void setEmp(int i);
 }
