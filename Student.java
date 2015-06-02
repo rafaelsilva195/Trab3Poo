78c7eec2b2ed2c62f7d5f4ef8d8d56d0e9320bfb
Rafael Bueno da Silva <rafaelzuum@hotmail.com>
2015-06-01 23:53:42 -0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,62 +1,21 @@
 package biblioteca;
 
 public class Student implements IUsers{
-	
 	String name;
 	int type;
-	boolean canEmp;
-	Books[] book = new Books[4];
-	int suspetionDays;
+	int numBooks;
 	
 	public Student(String name){
+		this.type = 7;
 		this.name = name;
-		this.type = 2;
-		book[0] = new Books("empyt");
-		book[1] = new Books("empyt");
-		book[2] = new Books("empyt");
-		book[3] = new Books("empyt");
-		this.suspetionDays = 0;
-		this.canEmp = true;
+		this.numBooks = 4;
 	}
 	
 	public int getType(){
 		return this.type;
 	}
 	
-	
-	public int suspetion(){
-		for(int i = 0; i < 4; i++){
-			if(suspetionDays < (book[i].numOfEmp()-15) && book[i].nameBook() != "empyt"){
-				suspetionDays = (book[i].numOfEmp()-15);
-				canEmp = false;					
-			}
-		}
-		return suspetionDays;
-	}
-	
-	public void setBook(String name){
-		if(this.canEmp == false){
-			System.out.println("Este usuário está suspenso!");
-			return;
-		}
-		for(int i = 0; i < 4; i++){
-			if(this.book[i].nameBook() == "empyt"){
-				System.out.println("O livro " + name + " foi emprestado");
-				this.book[i].setNameBook(name);
-				this.book[i].setNumOfEmp(15);
-				this.book[i].setUser(this.name);
-				return;
-			}
-		}
-		System.out.println("Este usuário não pode emprestar mais livros!");
-		return;
-	}
-	
-	public void getBook(){
-		for(int i = 0; i < 4; i++){
-			if(this.book[i].nameBook() != "empyt"){
-				System.out.println("Nome do livro : "+ this.book[i].nameBook());
-			}
-		}
+	public void setNumBooks(int numBooks){
+		this.numBooks = numBooks;
 	}
 }
